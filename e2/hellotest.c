6d79a1d7b77efcf6c3bae02781f0625c0a5ae1f0
wanglu <504021770@qq.com>
2019-04-18 16:12:32 +0800

    hehe

---
+++ e2/hellotest.c
@@ -0,0 +1,15 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h
+
+int main(void)
+{
+    int fd;
+    int val = 1;
+    fd = open("/dev/hellodev", O_RDWR);
+    if(fd < 0){
+        printf("can't open!\n");
+    }
+    write(fd, &val, 4);
+    return 0;
+}
